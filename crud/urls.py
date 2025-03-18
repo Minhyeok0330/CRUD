1e5b0cbbb0cc316cab3bc4cd29fd451043f99c4c
minyeok <alsgurgud@naver.com>
2025-03-18 12:19:04 +0900

    Read(1) 기능 구현

--- crud/urls.py
+++ crud/urls.py
@@ -21,4 +21,8 @@ from posts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index),
+    path('posts/<int:id>/', views.detail),
+    #posts/1/
+    #posts/10/
+    #posts/123/ 얘네들 다 내가 처리할테니까 걱정들 붙들어매쇼잉
 ]
